35fa81da36553b80a2a248343b2f634822056fd2
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-10-27 17:10:31 +0800

    Added Commit from Angular Bootstrapping.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ec-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'e-commerce';
+}
